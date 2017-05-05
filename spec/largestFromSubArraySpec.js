31b4503e0864b0db7a8219890f59847564b4163f
Rodion Zyuzev <Knoty01@gmail.com>
2017-05-05 18:57:20 +0300

    added all test cases from codecamp

--- spec/largestFromSubArraySpec.js
+++ spec/largestFromSubArraySpec.js
@@ -1,6 +1,6 @@
 describe("largestFromSubArray", function() {
   var array = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
-
+ 
   it("should return array", function() {
     var result = largestFromSubArray(array);
 	
@@ -12,4 +12,18 @@ describe("largestFromSubArray", function() {
 	
 	expect (result).toEqual([5, 27, 39, 1001]);
   });
+  
+  it ("should return [27,5,39,1001]", function() {
+	var result = largestFromSubArray([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+	
+	expect (result).toEqual([27,5,39,1001]);
+  });  
+  
+  it ("should return [9, 35, 97, 1000000]", function() {
+	var result = largestFromSubArray([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]);
+	
+	expect (result).toEqual([9, 35, 97, 1000000]);
+  });
+  
+  
 });
