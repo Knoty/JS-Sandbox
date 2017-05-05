0e6285ea6b7d55892b7c752a68fcd215151e8811
Rodion Zyuzev <Knoty01@gmail.com>
2017-05-05 18:59:44 +0300

    inlined unnecessary variable

--- spec/largestFromSubArraySpec.js
+++ spec/largestFromSubArraySpec.js
@@ -1,14 +1,13 @@
 describe("largestFromSubArray", function() {
-  var array = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
  
   it("should return array", function() {
-    var result = largestFromSubArray(array);
+    var result = largestFromSubArray([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 	
     expect(result instanceof Array).toBeTruthy();
   });
   
   it ("should return [5, 27, 39, 1001]", function() {
-	var result = largestFromSubArray(array);
+	var result = largestFromSubArray([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 	
 	expect (result).toEqual([5, 27, 39, 1001]);
   });
