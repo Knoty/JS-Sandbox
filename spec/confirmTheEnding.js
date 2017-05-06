f03c4610a4d5040081ef4a30cb2be756b73db51f
Rodion Zyuzev <Knoty01@gmail.com>
2017-05-06 17:06:54 +0300

    Removed character escaping.

--- spec/confirmTheEnding.js
+++ spec/confirmTheEnding.js
@@ -4,32 +4,32 @@ function confirmTheEnding(str, target) {
 
 describe("confirmTheEnding", function() {
 
-  it("\"Bastian\" and \"n\" should return true", function() {
+  it('"Bastian" and "n" should return true', function() {
     expect(confirmTheEnding("Bastian", "n")).toBeTruthy();
   });  
   
-  it("\"Connor\" and \"n\" should return false", function() {
+  it('"Connor" and "n" should return false', function() {
     expect(confirmTheEnding("Connor", "n")).toBeFalsy();
   });    
   
-  it("\"...both are frozen\" and \"specification\" should return false", function() {
+  it('"...both are frozen" and "specification" should return false', function() {
     expect(confirmTheEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")).toBeFalsy();
   });  
   
-  it("\"He has to give me a new name\" and \"name\" should return true", function() {
+  it('"He has to give me a new name" and "name" should return true', function() {
     expect(confirmTheEnding("He has to give me a new name", "name")).toBeTruthy();
   });  
   
-  it("\"Open sesame\" and \"same\" should return true", function() {
+  it('"Open sesame" and "same" should return true', function() {
     expect(confirmTheEnding("Open sesame", "same")).toBeTruthy();
   });  
   
-  it("\"Open sesame\" and \"pen\" should return false", function() {
+  it('"Open sesame" and "pen" should return false', function() {
     expect(confirmTheEnding("Open sesame", "pen")).toBeFalsy();
   });
   
-  it("\"...we can withstand the nothing\" and \"mountain\" should return false", function() {
-    expect(confirmTheEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain")).toBeFalsy();
+  it('"...we can withstand the nothing" and "mountain" should return false', function() {
+    expect(confirmTheEnding("If you want to save our world, you must hurry. We don't know how much longer we can withstand the nothing", "mountain")).toBeFalsy();
   });
 
 });
