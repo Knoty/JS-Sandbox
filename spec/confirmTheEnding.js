f28b2bbe1b066436fa0dd295efce7df2d73dc835
Rodion Zyuzev <Knoty01@gmail.com>
2017-05-06 17:26:09 +0300

    Simplified code using new generalized method for confirmTheEnding.

--- spec/confirmTheEnding.js
+++ spec/confirmTheEnding.js
@@ -6,34 +6,16 @@ describe("confirmTheEnding", function() {
 
   function itEndsWith(str, target, expected_result) { 
     it('"' + str + '" and "' + target + '" should return ' + expected_result, function() {
-    expect(confirmTheEnding(str,target)).toEqual(expected_result);
-  });  
+      expect(confirmTheEnding(str,target)).toEqual(expected_result);
+    });  
   }
 
   itEndsWith("Bastian", "n", true);
-  
-  it('"Connor" and "n" should return false', function() {
-    expect(confirmTheEnding("Connor", "n")).toBeFalsy();
-  });    
-  
-  it('"...both are frozen" and "specification" should return false', function() {
-    expect(confirmTheEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")).toBeFalsy();
-  });  
-  
-  it('"He has to give me a new name" and "name" should return true', function() {
-    expect(confirmTheEnding("He has to give me a new name", "name")).toBeTruthy();
-  });  
-  
-  it('"Open sesame" and "same" should return true', function() {
-    expect(confirmTheEnding("Open sesame", "same")).toBeTruthy();
-  });  
-  
-  it('"Open sesame" and "pen" should return false', function() {
-    expect(confirmTheEnding("Open sesame", "pen")).toBeFalsy();
-  });
-  
-  it('"...we can withstand the nothing" and "mountain" should return false', function() {
-    expect(confirmTheEnding("If you want to save our world, you must hurry. We don't know how much longer we can withstand the nothing", "mountain")).toBeFalsy();
-  });
+  itEndsWith("Connor", "n", false);
+  itEndsWith("Walking on water and developing software from a specification are easy if both are frozen", "specification", false);
+  itEndsWith("He has to give me a new name", "name", true);
+  itEndsWith("Open sesame", "same", true);
+  itEndsWith("Open sesame", "pen", false);
+  itEndsWith("If you want to save our world, you must hurry. We don't know how much longer we can withstand the nothing", "mountain", false);
 
 });
