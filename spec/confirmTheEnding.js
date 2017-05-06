c76a025e5c0f6379383b2ec84dc3895369db7c5d
Rodion Zyuzev <Knoty01@gmail.com>
2017-05-06 17:02:39 +0300

    Added confirmTheEnding function and specs to it

---
+++ spec/confirmTheEnding.js
@@ -0,0 +1,35 @@
+function confirmTheEnding(str, target) {
+    return str.search(target+'$') >= 0;
+}
+
+describe("confirmTheEnding", function() {
+
+  it("\"Bastian\" and \"n\" should return true", function() {
+    expect(confirmTheEnding("Bastian", "n")).toBeTruthy();
+  });  
+  
+  it("\"Connor\" and \"n\" should return false", function() {
+    expect(confirmTheEnding("Connor", "n")).toBeFalsy();
+  });    
+  
+  it("\"...both are frozen\" and \"specification\" should return false", function() {
+    expect(confirmTheEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")).toBeFalsy();
+  });  
+  
+  it("\"He has to give me a new name\" and \"name\" should return true", function() {
+    expect(confirmTheEnding("He has to give me a new name", "name")).toBeTruthy();
+  });  
+  
+  it("\"Open sesame\" and \"same\" should return true", function() {
+    expect(confirmTheEnding("Open sesame", "same")).toBeTruthy();
+  });  
+  
+  it("\"Open sesame\" and \"pen\" should return false", function() {
+    expect(confirmTheEnding("Open sesame", "pen")).toBeFalsy();
+  });
+  
+  it("\"...we can withstand the nothing\" and \"mountain\" should return false", function() {
+    expect(confirmTheEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain")).toBeFalsy();
+  });
+
+});
